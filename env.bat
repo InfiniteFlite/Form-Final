fb41ba8ed199ffa80c5b661a91c556a8f8b676e7
Infinite Flite <infinate1234flight@gmail.com>
2022-05-12 10:02:05 -0700

    formatting more

--- env.bat
+++
@@ -1,5 +0,0 @@
-set SECRET_KEY=xxgf/xgh/xir/xqyu/xue4/xue/xntrn/x464f/cxh/xjy/xqhyc/xwe6f/x6rfbc/4ix8xbx5xjx/ewrt
-set MONGO_DBNAME=CarForm
-set MONGO_CONNECTION_STRING=mongodb+srv://Admin:F1vAqBaNOY0C1kRA@dswcluster.v9owr.mongodb.net/CarForm?retryWrites=true
-set GITHUB_CLIENT_ID=b255d38a4c615204774c
-set GITHUB_CLIENT_SECRET=cfcdada6ac93f7aea85e9f0319941a2368b2d714
